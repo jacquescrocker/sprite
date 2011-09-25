78ed11c36679fa0ddee3e344321f0cc3c8730aa9
Jacques Crocker <railsjedi@gmail.com>
2011-09-25 08:09:32 -0700

    adding stylus generator

--- lib/sprite/styles.rb
+++ lib/sprite/styles.rb
@@ -2,15 +2,18 @@ require 'sprite/styles/sass_generator'
 require 'sprite/styles/css_generator'
 require 'sprite/styles/sass_yml_generator'
 require 'sprite/styles/sass_mixin_generator'
+require 'sprite/styles/stylus_generator'
 require 'sprite/styles/templated_css_generator'
 
 module Sprite::Styles
+
   GENERATORS = {
     "css" => "CssGenerator",
     "templated_css" => "TemplatedCssGenerator",
     "sass" => "SassGenerator",
     "sass_mixin" => "SassMixinGenerator",
-    "sass_yml" => "SassYmlGenerator"
+    "sass_yml" => "SassYmlGenerator",
+    "stylus" => "StylusGenerator",
   }
 
   def self.get(config)
---
+++ lib/sprite/styles/stylus_generator.rb
@@ -0,0 +1,45 @@
+module Sprite
+  module Styles
+    # renders a yml file that is later used by stylus when generating mixins
+    class StylusGenerator
+      def initialize(builder)
+        @builder = builder
+      end
+
+      def write(path, sprite_files)
+        # write the stylus mixins to disk
+        File.open(File.join(Sprite.root, path), 'w') do |f|
+          add_else = false
+
+          f.puts "sprite(group_name, image_name, offset = 0)"
+          sprite_files.each do |sprite_file, sprites|
+            background_url = @builder.background_url(sprite_file)
+            sprites.each do |sprite|
+              f << "  "
+              if add_else
+                f << "else "
+              end
+              add_else = true
+
+              if sprite[:align] == 'horizontal'
+                background_offset = "{#{sprite[:x]}+offset}px #{sprite[:y]}px"
+              else
+                background_offset = "#{sprite[:x]}px {#{sprite[:y]}+offset}px"
+              end
+
+              f.puts %{if group_name == "#{sprite[:group]}" and image_name == "#{sprite[:name]}"}
+              f.puts "    background: #{background_url} repeat #{background_offset}"
+              f.puts "    width: #{sprite[:width]}px"
+              f.puts "    height: #{sprite[:height]}px"
+            end
+          end
+        end
+      end
+
+      def extension
+        "styl"
+      end
+
+    end
+  end
+end
