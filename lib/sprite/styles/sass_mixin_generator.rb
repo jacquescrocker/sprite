4af94bcf83d717e5ae3ff8068246186eeece48b4
Jacques Crocker <railsjedi@gmail.com>
2012-06-22 22:28:26 -0700

    add additional sprite-dimensions mixin

--- lib/sprite/styles/sass_mixin_generator.rb
+++ lib/sprite/styles/sass_mixin_generator.rb
@@ -10,7 +10,7 @@ module Sprite
         # write the sass mixins to disk
         File.open(File.join(Sprite.root, path), 'w') do |f|
 
-          f.puts "= sprite($group_name, $image_name, $offset: 0)"
+          f.puts "= sprite($group-name, $image-name, $offset: 0)"
           sprite_files.each do |sprite_file, sprites|
             background_url = @builder.background_url(sprite_file)
             sprites.each do |sprite|
@@ -23,8 +23,18 @@ module Sprite
                 background_offset = "#{sprite[:x]}px \#{#{sprite[:y]}+$offset}px"
               end
 
-              f.puts %{if $group_name == "#{sprite[:group]}" and $image_name == "#{sprite[:name]}"}
+              f.puts %{if $group-name == "#{sprite[:group]}" and $image-name == "#{sprite[:name]}"}
               f.puts "    background: #{background_url} no-repeat #{background_offset}"
+              f.puts "    +sprite-dimensions($group-name, $image-name)"
+            end
+          end
+
+          f.puts "\n= sprite-dimensions($group-name, $image-name)"
+          sprite_files.each do |sprite_file, sprites|
+            background_url = @builder.background_url(sprite_file)
+            sprites.each do |sprite|
+              f << "  @"
+              f.puts %{if $group-name == "#{sprite[:group]}" and $image-name == "#{sprite[:name]}"}
               f.puts "    width: #{sprite[:width]}px"
               f.puts "    height: #{sprite[:height]}px"
             end
